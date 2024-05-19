565fc5e89a3b9dbb5d87ddf094ea677b2d612616
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2024-05-20 00:33:39 +0530

    Handling form input validation with native default props

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 
 // * Handled form data using native formData constructor.
 export default function Signup() {
-
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
+ 
   function handleSubmission(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
-    console.log("🚀 ~ handleSubmission ~ formData:", formData);
     const acquisitionChannel = formData.getAll("acquisition");
-    console.log("🚀 ~ handleSubmission ~ acquisitionChannel:", acquisitionChannel)
     const data = Object.fromEntries(formData.entries());
-    data.acquisition = acquisitionChannel;
+    data.acquisition = acquisitionC
+    
+    // * Creating custom validation for form inputs
+    if (data.password !== data['confirm-password']) {
+      setPasswordsAreNotEqual(true);
+      return;
+    }
+
+    setPasswordsAreNotEqual(false);
     console.log("🚀 ~ handleSubmission ~ data:", data);
   }
 
@@ -20,13 +28,13 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" required/>
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" required  minLength={6}/>
         </div>
 
         <div className="control">
@@ -35,7 +43,11 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            required
           />
+          <div className="control-error">
+            {passwordsAreNotEqual && <p>Passwords should match.</p>}
+          </div>
         </div>
       </div>
 
@@ -44,18 +56,18 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name"  required/>
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" required />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select id="role" name="role" required >
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -94,7 +106,7 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          <input type="checkbox" id="terms-and-conditions" name="terms"  required />I
           agree to the terms and conditions
         </label>
       </div>
